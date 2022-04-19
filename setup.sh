5936c28ffc0aa79b610daba381a6cbb1c12425a0
harsh7839 <100824266+harsh7839@users.noreply.github.com>
2022-04-19 11:17:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,3 +13,15 @@ else
     echo "Allow Storage !"
     termux-setup-storage
 fi
+
+cd termux-setup
+apt install curl
+
+REPO='repo'
+if [ -f "$REPO" ]; then
+    echo "Repository Already Changed !"
+else
+    echo "Change Your Termux Repository !"
+    termux-change-repo
+    curl -OL https://raw.githubusercontent.com/harsh7839/termux-setup-files/main/repo
+fi
