e72c3dfac3afb6d65c0a12af6ace9023784e9529
harsh7839 <100824266+harsh7839@users.noreply.github.com>
2022-04-19 10:58:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,15 @@
 
+echo "
+╦  ╔═╗╔╦╗╔═╗  ╔╗ ╔═╗╔═╗╦╔╗╔
+║  ║╣  ║ ╚═╗  ╠╩╗║╣ ║ ╦║║║║
+╩═╝╚═╝ ╩ ╚═╝  ╚═╝╚═╝╚═╝╩╝╚╝
+"
+
+cd ..
+STORAGE='storage'
+if [ -d "$STORAGE" ]; then
+    echo "Storage Setup Already Satisfied !"
+else
+    echo "Allow Storage !"
+    termux-setup-storage
+fi
